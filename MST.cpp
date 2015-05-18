3a170ebe52c41a68097e1930612c082de8c8ad01
luis maltos <maltos@localhost.localdomain>
2015-05-18 09:27:03 -0500

    Rutinas para MST

---
+++ MST.cpp
@@ -0,0 +1,94 @@
+// A C / C++ program for Prim's Minimum Spanning Tree (MST) algorithm.
+// The program is for adjacency matrix representation of the graph
+ 
+#include <stdio.h>
+#include <limits.h>
+ 
+// A utility function to find the vertex with minimum key value, from
+// the set of vertices not yet included in MST
+int minKey(int V,int *key, bool *mstSet)
+{
+   // Initialize min value
+   int min = INT_MAX, min_index;
+ 
+   for (int v = 0; v < V; v++)
+     if (mstSet[v] == false && key[v] < min)
+         min = key[v], min_index = v;
+ 
+   return min_index;
+}
+ 
+// A utility function to print the constructed MST stored in parent[]
+int printMST(int V,int *parent, int **graph)
+{
+   printf("Edge   Weight\n");
+   for (int i = 1; i < V; i++)
+      printf("%d - %d    %d \n", parent[i], i, graph[i][parent[i]]);
+}
+ 
+// Function to construct and print MST for a graph represented using adjacency
+// matrix representation
+int* primMST(int V,int **graph){
+  int *parent; // Array to store constructed MST
+  int *key;   // Key values used to pick minimum weight edge in cut
+  bool *mstSet;  // To represent set of vertices not yet included in MST
+
+  parent = new int[V];
+  key  = new int[V];
+  mstSet = new bool[V];
+ 
+  // Initialize all keys as INFINITE
+  for (int i = 0; i < V; i++)
+    key[i] = INT_MAX, mstSet[i] = false;
+ 
+  // Always include first 1st vertex in MST.
+  key[0] = 0;     // Make key 0 so that this vertex is picked as first vertex
+  parent[0] = -1; // First node is always root of MST
+ 
+  // The MST will have V vertices
+  for (int count = 0; count < V-1; count++) {
+    // Pick thd minimum key vertex from the set of vertices
+    // not yet included in MST
+    int u = minKey(V,key, mstSet);
+ 
+    // Add the picked vertex to the MST Set
+    mstSet[u] = true;
+ 
+    // Update key value and parent index of the adjacent vertices of
+    // the picked vertex. Consider only those vertices which are not yet
+    // included in MST
+    for (int v = 0; v < V; v++) {
+      // graph[u][v] is non zero only for adjacent vertices of m
+      // mstSet[v] is false for vertices not yet included in MST
+      // Update the key only if graph[u][v] is smaller than key[v]
+      if (graph[u][v] && mstSet[v] == false && graph[u][v] <  key[v])
+	parent[v]  = u, key[v] = graph[u][v];
+    }
+  }
+  delete mstSet[];
+  delete key;
+  retrun parent;
+}
+ 
+ 
+// driver program to test above function
+int main() {
+   /* Let us create the following graph
+          2    3
+      (0)--(1)--(2)
+       |   / \   |
+      6| 8/   \5 |7
+       | /     \ |
+      (3)-------(4)
+            9          */
+  int *parent;
+  int graph[5][5] = {{0, 2, 0, 6, 0},
+		     {2, 0, 3, 8, 5},
+		     {0, 3, 0, 0, 7},
+		     {6, 8, 0, 0, 9},
+		     {0, 5, 7, 9, 0},
+                     };
+  parent = primMST(5,graph);
+  printMST(5,parent,graph);
+  return 0;
+}
